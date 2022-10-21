bb5fbae6f4ea536be0222f25ebf8c11a63aec036
lucyoz <lucyoz.kwon@gmail.com>
2022-10-21 10:44:14 +0900

    괄호풀기 split이용, 스택 이용

--- src/main/java/programmers/SolveBracker.java
+++ src/main/java/programmers/SolveBracker.java
@@ -1,8 +1,11 @@
 package programmers;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 public class SolveBracker {
     //replace 이용
-    public boolean solution(String s){
+    public boolean solution1(String s){
         while(s.indexOf("()")>=0){
             s = s.replaceAll("\\(\\)","");
 
@@ -10,10 +13,27 @@ public class SolveBracker {
         return s.length()==0;
     }
 
-    public static void main(String[] args) {
-        String s = "()()";
+    public boolean solution2(String s){
+        while(s.indexOf("()")>=0){
+            String[] splitted = s.split("\\(\\)");
+            s = String.join("",splitted);
+        }
 
+        return s.length()==0;
+    }
 
+    public boolean solution(String s){
+        Stack<Character> st = new Stack<>();
 
+        for(int i=0;i<s.length();i++){
+            if('('== (s.charAt(i))){
+                st.push(s.charAt(i));
+            } else if(')' == s.charAt(i)){
+                if(st.empty())  return false;
+                st.pop();
+            }
+        }
+
+        return st.empty();
     }
 }
