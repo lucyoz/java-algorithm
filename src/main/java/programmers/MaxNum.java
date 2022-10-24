9cfb9514297544fa4bab2fcdbb768b84eef2f1b7
lucyoz <lucyoz.kwon@gmail.com>
2022-10-24 10:12:36 +0900

    getKthNum using priorityQueue

---
+++ src/main/java/programmers/MaxNum.java
@@ -0,0 +1,16 @@
+package programmers;
+
+public class MaxNum {
+    public String solution(int[] numbers) {
+        String answer = "";
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
