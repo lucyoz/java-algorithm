0d758b7b184caba08e78cdaac7433ec66c9d1ab6
lucyoz <lucyoz.kwon@gmail.com>
2022-10-25 10:16:18 +0900

    HashTable 실습 1. hash 2. insert 3. search

---
+++ src/main/java/com/likelion/hash/HashTable2.java
@@ -0,0 +1,44 @@
+package com.likelion.hash;
+
+public class HashTable2 {
+    private int size = 10000;
+    private int[] table = new int[size];
+
+    public HashTable2(){}
+    public HashTable2(int size) {
+        this.size = size;
+        this.table = new int[size];
+    }
+    public int hash(String key){
+        int asciiSum = 0;
+        for (int i = 0; i < key.length(); i++) {
+            asciiSum += key.charAt(i);
+        }
+        return asciiSum%size;
+
+    }
+    public void insert(String key, Integer value){
+        int hashcode = hash(key);
+        this.table[hashcode] = value;
+        System.out.println(hashcode+"방에 저장되었습니다");
+    }
+
+    public int search(String key){
+        return this.table[hash(key)];
+    }
+    public static void main(String[] args) {
+        String[] names = new String[]{"DongyeonKang",
+                "SubinKang", "KwanwunKo", "HyunseokKo", "KyoungdukKoo", "YeonjiGu", "SoyeonKown", "OhsukKwon", "GunwooKim", "KiheonKim", "NayeongKim", "DohyeonKim", "MinkyoungKim", "MinjiKim", "SanghoKim", "SolbaeKim", "YejinKim", "EungjunKim", "JaegeunKim", "JeonghyeonKim", "JunhoKim", "JisuKim", "kimjinah", "HaneulKim", "HeejungKim", "KimoonPark", "EunbinPark", "JeongHoonPark", "JeminPark", "TaegeunPark", "JiwonBae", "SeunggeunBaek", "JihwanByeon", "HeungseopByeon", "JeongHeeSeo", "TaegeonSeo", "SeeYunSeok", "SuyeonSeong", "SeyoelSon", "MinjiSong", "JinwooSong", "hyunboSim", "SominAhn", "JiyoungAhn", "ChangbumAn", "SoonminEom",
+                "HyeongsangOh", "SuinWoo", "JuwanWoo", "InkyuYoon", "GahyunLee", "DaonLee", "DohyunLee", "SanghunLee", "SujinLee", "AjinLee", "YeonJae", "HyeonjuLee", "HakjunYim", "SeoyunJang", "SeohyeonJang", "JinseonJang", "SujinJeon", "SeunghwanJeon", "DaehwanJung", "JaeHyunJeung", "HeejunJeong", "GukhyeonCho", "MunjuJo", "YejiJo", "ChanminJu", "MinjunChoi", "SujeongChoi", "SeunghoChoi", "AyeongChoi", "GeonjooHan", "JinhyuckHeo", "MinwooHwang", "SieunHwang",
+                "JunhaHwang"};
+
+        HashTable2 ht = new HashTable2(200);
+
+        for (int i = 0; i < names.length; i++) {
+            ht.insert(names[i], ht.hash(names[i]));
+        }
+
+        System.out.println(ht.search("DongyeonKan"));
+        System.out.println(ht.search("JiyoungAhn"));
+    }
+}
