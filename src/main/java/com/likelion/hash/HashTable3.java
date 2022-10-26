3b35708b1e2635f444a23babb49ef0d8f7d477b6
lucyoz <lucyoz.kwon@gmail.com>
2022-10-26 11:02:58 +0900

    HashTable 실습

---
+++ src/main/java/com/likelion/hash/HashTable3.java
@@ -0,0 +1,65 @@
+package com.likelion.hash;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HashTable3 {
+    class Node{
+        private String key;
+        private Integer value;
+
+        public Node(String key, Integer value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public Integer getValue() {
+            return value;
+        }
+    }
+    List<Node>[] table = new ArrayList[1000];
+
+    public int hash(String key){
+        int asciiNum = 0;
+        for (int i = 0; i < key.length(); i++) {
+            asciiNum += key.charAt(i);
+        }
+        return asciiNum%1000;
+    }
+
+    public void insert(String key, Integer value){
+        int hashIdx = hash(key);
+        if (this.table[hashIdx]==null){
+            this.table[hashIdx] = new ArrayList<>();
+        }
+        this.table[hashIdx].add(new Node(key, value));
+        System.out.println(key+" "+hashIdx+"방에 저장되었습니다");
+    }
+
+    public Integer search(String key){
+        List<Node> nodes = this.table[hash(key)];
+        for(Node node:nodes){
+            if(key.equals(node.getKey())){
+                return node.getValue();
+            }
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        HashTable3 ht = new HashTable3();
+        ht.insert("Yoonseo",1);
+        ht.insert("Seoyoon",2);
+
+        if(ht.search("Yoonseo")==1 &&ht.search("Seoyoon")==2){
+            System.out.println("테스트성공");
+        } else {
+            System.out.println("테스트 실패");
+        }
+    }
+}
