6ee96a4d5ff1f52b9fe31a605ec140e36a8c0ca4
lucyoz <lucyoz.kwon@gmail.com>
2022-10-26 10:43:20 +0900

    HashTable

--- src/main/java/com/likelion/hash/HashTable2.java
+++ src/main/java/com/likelion/hash/HashTable2.java
@@ -1,44 +1,65 @@
 package com.likelion.hash;
 
+import programmers.Solution42576;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HashTable2 {
-    private int size = 10000;
-    private int[] table = new int[size];
+    class Node{
+        private String key;
+        private Integer value;
 
-    public HashTable2(){}
-    public HashTable2(int size) {
-        this.size = size;
-        this.table = new int[size];
-    }
-    public int hash(String key){
-        int asciiSum = 0;
-        for (int i = 0; i < key.length(); i++) {
-            asciiSum += key.charAt(i);
+        public Node(String key, Integer value) {
+            this.key = key;
+            this.value = value;
         }
-        return asciiSum%size;
 
+        public String getKey() {
+            return key;
+        }
+
+        public Integer getValue() {
+            return value;
+        }
+    }
+    List<Node>[] table = new ArrayList[1000];
+
+    public int hash(String key){
+        int asciiNum = 0;
+        for (int i = 0; i < key.length(); i++) {
+            asciiNum += key.charAt(i);
+        }
+        return asciiNum%1000;
     }
     public void insert(String key, Integer value){
-        int hashcode = hash(key);
-        this.table[hashcode] = value;
-        System.out.println(hashcode+"방에 저장되었습니다");
+        int hashIdx = hash(key);
+        if(this.table[hashIdx]==null){
+            this.table[hashIdx] = new ArrayList<>();
+        }
+        //Map, Object
+        this.table[hashIdx].add(new Node(key, value));
+        System.out.println(key + " "+hashIdx+"방에 저장이 완료되었습니다.");
     }
 
-    public int search(String key){
-        return this.table[hash(key)];
+    public Integer search(String key){
+        List<Node> nodes = this.table[hash(key)];
+        for (Node node: nodes) {
+            if(key.equals(node.getKey())){
+                return node.getValue();
+            }
+        }
+        return null;
     }
     public static void main(String[] args) {
-        String[] names = new String[]{"DongyeonKang",
-                "SubinKang", "KwanwunKo", "HyunseokKo", "KyoungdukKoo", "YeonjiGu", "SoyeonKown", "OhsukKwon", "GunwooKim", "KiheonKim", "NayeongKim", "DohyeonKim", "MinkyoungKim", "MinjiKim", "SanghoKim", "SolbaeKim", "YejinKim", "EungjunKim", "JaegeunKim", "JeonghyeonKim", "JunhoKim", "JisuKim", "kimjinah", "HaneulKim", "HeejungKim", "KimoonPark", "EunbinPark", "JeongHoonPark", "JeminPark", "TaegeunPark", "JiwonBae", "SeunggeunBaek", "JihwanByeon", "HeungseopByeon", "JeongHeeSeo", "TaegeonSeo", "SeeYunSeok", "SuyeonSeong", "SeyoelSon", "MinjiSong", "JinwooSong", "hyunboSim", "SominAhn", "JiyoungAhn", "ChangbumAn", "SoonminEom",
-                "HyeongsangOh", "SuinWoo", "JuwanWoo", "InkyuYoon", "GahyunLee", "DaonLee", "DohyunLee", "SanghunLee", "SujinLee", "AjinLee", "YeonJae", "HyeonjuLee", "HakjunYim", "SeoyunJang", "SeohyeonJang", "JinseonJang", "SujinJeon", "SeunghwanJeon", "DaehwanJung", "JaeHyunJeung", "HeejunJeong", "GukhyeonCho", "MunjuJo", "YejiJo", "ChanminJu", "MinjunChoi", "SujeongChoi", "SeunghoChoi", "AyeongChoi", "GeonjooHan", "JinhyuckHeo", "MinwooHwang", "SieunHwang",
-                "JunhaHwang"};
+        HashTable2 ht = new HashTable2();
+        ht.insert("Yoonseo",1);
+        ht.insert("Seoyoon",2);
 
-        HashTable2 ht = new HashTable2(200);
-
-        for (int i = 0; i < names.length; i++) {
-            ht.insert(names[i], ht.hash(names[i]));
+        if(ht.search("Yoonseo")==1 &&ht.search("Seoyoon")==2){
+            System.out.println("테스트성공");
+        } else {
+            System.out.println("테스트 실패");
         }
-
-        System.out.println(ht.search("DongyeonKan"));
-        System.out.println(ht.search("JiyoungAhn"));
     }
 }
