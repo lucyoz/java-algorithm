c8705ef98fa9be4398d661e9e606dd9cfb093fbc
lucyoz <lucyoz.kwon@gmail.com>
2022-10-27 10:34:06 +0900

    완주하지 못한 선수

--- src/main/java/programmers/ARacer3.java
+++ src/main/java/programmers/ARacer3.java
@@ -3,33 +3,30 @@ package programmers;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ARacer2 {
-    //HashMap을 이용할 수 있다
-
+public class ARacer3 {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         Map<String, Integer> memo = new HashMap<>();
 
         for (int i = 0; i < participant.length; i++) {
-            //바로 put을 할 수 없고 숫자 올려야한다.
             String key = participant[i];
-            if(!memo.containsKey(key)){
+            if(memo.containsKey(key)){
                 memo.put(key, 0);
             }
             memo.put(key, memo.get(key)+1);
-
         }
+
         for (int i = 0; i < completion.length; i++) {
             String key = completion[i];
-            memo.put(key, memo.get(key)-1);
+            memo.put(key,memo.get(key)-1);
         }
-        for(String key: memo.keySet()){
+
+        for(String key:memo.keySet()){
             if(memo.get(key)==1){
                 answer = key;
             }
+
         }
-
-
         return answer;
     }
 
