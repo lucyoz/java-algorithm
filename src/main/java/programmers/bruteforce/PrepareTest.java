0f77ad77956afc20e7f4c52431bc8786489e7698
lucyoz <lucyoz.kwon@gmail.com>
2022-10-31 09:40:54 +0900

    모의고사 - 세 학생의 점수 구하기

--- src/main/java/programmers/bruteforce/PrepareTest.java
+++ src/main/java/programmers/bruteforce/PrepareTest.java
@@ -8,28 +8,37 @@ public class PrepareTest {
         3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
        */
 
-        int firstStudentAnswerCnt = 0;
 
         int[] firstStudent = {1, 2, 3, 4, 5};
         int[] secondStudent = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] thirdStudent = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
-        for(int i=0;i<answer.length;i++){
-            if (i>firstStudent.length){
-                firstStudent[i] = firstStudent[i%5];
-            }
-            if(answer[i] == firstStudent[i]){
-                firstStudentAnswerCnt++;
-            }
-        }
+        int firstStudentAnswerCnt =StudentAnswerCnt(firstStudent, answer);
+        int secondStudentAnswerCnt =StudentAnswerCnt(secondStudent, answer);
+        int thirdStudentAnswerCnt =StudentAnswerCnt(thirdStudent, answer);
 
         System.out.println(firstStudentAnswerCnt);
+        System.out.println(secondStudentAnswerCnt);
+        System.out.println(thirdStudentAnswerCnt);
         return new int[0];
+    }
+
+    public int StudentAnswerCnt(int[] student, int[] answer){
+        int StudentAnswerCnt = 0;
+        for(int i=0;i<answer.length;i++){
+            if (i>student.length){
+                student[i] = student[i%student.length];
+            }
+            if(answer[i] == student[i]){
+                StudentAnswerCnt++;
+            }
+        }
+        return StudentAnswerCnt;
     }
 
     public static void main(String[] args) {
         PrepareTest pt = new PrepareTest();
-        int[] answer = {1, 3, 2, 4, 2};
+        int[] answer = {1, 2, 3, 4, 5};
         pt.solution(answer);
     }
 }
