f747ea41303dcded4a62539dd0998205c8920f44
lucyoz <lucyoz.kwon@gmail.com>
2022-11-02 09:32:24 +0900

    List를 이용하여 에라토스테네스의 체 방법으로 소수 찾기

---
+++ src/main/java/programmers/bruteforce/RemoveMultipleOf.java
@@ -0,0 +1,31 @@
+package programmers.bruteforce;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RemoveMultipleOf {
+
+
+    public static void main(String[] args) {
+        int N = 50;   //50미만의 모든 소수를 구하기
+
+        // 2부터 N까지 숫자가 들어있는 List만들기
+        List<Integer> NList = new ArrayList<>();
+        for(int i=2;i<=N;i++){
+            NList.add(i);
+        }
+
+        int Nsqrt = (int) Math.sqrt(N);
+
+        for(int i=2;i<=Nsqrt;i++){
+            for(int j=0;j<NList.size();j++){
+                if (NList.get(j) % i == 0 && NList.get(j) != i){
+                    NList.remove(j);
+                }
+            }
+        }
+
+        System.out.println(NList.size());
+
+    }
+}
