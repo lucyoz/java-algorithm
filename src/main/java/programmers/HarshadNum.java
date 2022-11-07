364ad2676a08fd98dab49f4f2124de561629f30d
lucyoz <lucyoz.kwon@gmail.com>
2022-11-07 09:24:42 +0900

    하샤드 수 1. sumOfDigit 2. harShad

--- src/main/java/programmers/HarshadNum.java
+++ src/main/java/programmers/HarshadNum.java
@@ -1,19 +1,17 @@
 package programmers;
 
 public class HarshadNum {
-    public int sumNum(int num){
-        int sum = 0;
-        int i = 1;
+    public int sum(int num){
+        int sumOfDigit = 0;
         while(num>0){
-            sum += num % (int)Math.pow(10,i);
-            num /= Math.pow(10,i);
-            i++;
+            sumOfDigit += num % 10;
+            num /= 10;
         }
-        System.out.println(sum);
-        return sum;
+        System.out.println(sumOfDigit);
+        return sumOfDigit;
     }
     public boolean solution(int x){
-        if (x%sumNum(x)==0){
+        if (x%sum(x)==0){
             return true;
         }
         return false;
@@ -24,7 +22,7 @@ public class HarshadNum {
 
         HarshadNum harshadNum = new HarshadNum();
 
-        boolean answer = harshadNum.solution(81);
+        boolean answer = harshadNum.solution(687);
         System.out.println(answer);
     }
 }
