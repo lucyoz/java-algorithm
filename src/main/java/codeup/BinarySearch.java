c653d054502ec5f01918b2bf92e25a2ec093736f
lucyoz <lucyoz.kwon@gmail.com>
2022-11-08 11:03:52 +0900

    BinarySearch

--- src/main/java/codeup/BinarySearch.java
+++ src/main/java/codeup/BinarySearch.java
@@ -4,19 +4,19 @@ public class BinarySearch {
     public static void main(String[] args) {
         int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
         int targetNum = 7;
-        int startIdx = 0, endIdx = nums.length;
+        int startIdx = 0, endIdx = nums.length - 1;
 
-        while(true){
+        while(startIdx<=endIdx){
             //중간값 찾기
             int midIdx = startIdx + (endIdx-startIdx) / 2;
 
             //인덱스(시작, 끝점) 옮기기기
             if (targetNum < nums[midIdx]){
-                endIdx = midIdx;
+                endIdx = midIdx + 1;
             } else if (targetNum > nums[midIdx]){
-                startIdx = midIdx;
+                startIdx = midIdx - 1;
             } else {
-                System.out.println(midIdx+1);
+                System.out.println(midIdx);
                 break;
             }
 
