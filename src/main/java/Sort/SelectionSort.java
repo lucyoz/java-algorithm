0f465aa0f545319bb0fa9ace7c278367b43afd6a
lucyoz <lucyoz.kwon@gmail.com>
2022-11-09 09:39:24 +0900

    SelectionSort 오름차순으로

---
+++ src/main/java/Sort/SelectionSort.java
@@ -0,0 +1,25 @@
+package Sort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
+
+        for (int i = 0; i < arr.length; i++) {
+            int minIdx = i;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[minIdx]>arr[j]){
+                    minIdx = j;
+                }
+            }
+            int tmp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = tmp;
+
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
