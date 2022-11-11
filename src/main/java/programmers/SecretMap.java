94b92fcc2611239774c8c33f27340844f8ade3eb
lucyoz <lucyoz.kwon@gmail.com>
2022-11-11 09:10:31 +0900

    비밀지도 - 설계

---
+++ src/main/java/programmers/SecretMap.java
@@ -0,0 +1,12 @@
+package programmers;
+
+public class SecretMap {
+    //각 array의 요소 값들을 2진수로 바꾼 배열 만들기
+    // arr1과 arr2 같은 행끼리 OR 연산
+    // #을 1로, 공백을 " "으로
+
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = {};
+        return answer;
+    }
+}
