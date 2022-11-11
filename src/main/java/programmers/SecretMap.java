17a9cc42651de867b380489daa5b2a5f082c6e17
lucyoz <lucyoz.kwon@gmail.com>
2022-11-11 10:21:54 +0900

    비밀지도 - 개수에 맞는 이진수 배열 통해 문제 해결(repeat이용)[완료]

--- src/main/java/programmers/SecretMap.java
+++ src/main/java/programmers/SecretMap.java
@@ -7,12 +7,23 @@ public class SecretMap {
     // arr1과 arr2 같은 idx끼리 OR 연산
     // 1을 #로, 0을 " "으로
 
+    //10진수를 2진수로 만드는 함수
+    String getBinaryString(int decNum){
+        String answer = "";
+        while(decNum > 0){
+            answer += decNum % 2;
+            decNum /= 2;
+        }
+        return answer;
+    }
+
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
 
 
-        for (int i = 0; i < arr1.length; i++) {
+        for (int i = 0; i < n; i++) {
             String bin = Integer.toBinaryString(arr1[i] | arr2[i]);
+            //bin = "0".repeat(n-bin.length())+bin;
             bin = bin.replace("1","#");
             bin = bin.replace("0"," ");
             answer[i] = bin;
