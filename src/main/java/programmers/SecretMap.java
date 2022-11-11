f1e2482946408a6a65d4f1aca1428a1f3196d906
lucyoz <lucyoz.kwon@gmail.com>
2022-11-11 09:32:40 +0900

    비밀지도 - 프로그래머스 테스트1 통과, 2실패 (해결중)

--- src/main/java/programmers/SecretMap.java
+++ src/main/java/programmers/SecretMap.java
@@ -1,12 +1,32 @@
 package programmers;
 
+import java.util.Arrays;
+
 public class SecretMap {
-    //각 array의 요소 값들을 2진수로 바꾼 배열 만들기
-    // arr1과 arr2 같은 행끼리 OR 연산
-    // #을 1로, 공백을 " "으로
+    //각 array의 요소 값들을 2진수로 바꾸기 -> 바꾸지 않아도 됨!?
+    // arr1과 arr2 같은 idx끼리 OR 연산
+    // 1을 #로, 0을 " "으로
 
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = {};
+        String[] answer = new String[n];
+
+
+        for (int i = 0; i < arr1.length; i++) {
+            String bin = Integer.toBinaryString(arr1[i] | arr2[i]);
+            bin = bin.replace("1","#");
+            bin = bin.replace("0"," ");
+            answer[i] = bin;
+        }
+
         return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {9, 20, 28, 18, 11};
+        int[] arr2 = {30, 1, 21, 17, 28};
+
+        SecretMap secretMap = new SecretMap();
+        String[] answer = secretMap.solution(5,arr1,arr2);
+        System.out.println(Arrays.toString(answer));
     }
 }
