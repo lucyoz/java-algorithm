bcc241063ff273d12ae6b43834eaa4d643c6b954
lucyoz <lucyoz.kwon@gmail.com>
2022-11-22 09:23:21 +0900

    [UPDATE]재귀: 1부터 100까지 print - 탈출조건 넣기

--- src/main/java/recursive/Print1To100.java
+++ src/main/java/recursive/Print1To100.java
@@ -2,6 +2,7 @@ package recursive;
 
 public class Print1To100 {
     public static void print1To100(int n){
+        if(n>100) return;
         System.out.println(n);
         print1To100(n);
     }
