9c96439bc6db84c827e9622e82422e457be4f8e4
lucyoz <lucyoz.kwon@gmail.com>
2022-11-22 09:29:01 +0900

    [UPDATE]재귀: 1부터 100까지 print - 1씩 커지는 로직 넣기

--- src/main/java/recursive/Print1To100.java
+++ src/main/java/recursive/Print1To100.java
@@ -2,12 +2,12 @@ package recursive;
 
 public class Print1To100 {
     public static void print1To100(int n){
-        if(n>100) return;
+        if(n>100) return;           // 여기에 도달 할 수 있게 n을 증가시켜야
         System.out.println(n);
-        print1To100(n);
+        print1To100(n+1);
     }
 
     public static void main(String[] args) {
-        print1To100(1);
+        print1To100(1);         //출발을 1에서 함.
     }
 }
