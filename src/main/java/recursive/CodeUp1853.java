8ac40390adfb369a2b495c370d596b16c6e647cd
lucyoz <lucyoz.kwon@gmail.com>
2022-11-23 09:26:26 +0900

    [ADD]재귀: CodeUp 1853 - 재귀로 1부터 n까지 합 구하기

---
+++ src/main/java/recursive/CodeUp1853.java
@@ -0,0 +1,21 @@
+package recursive;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CodeUp1853 {
+
+    public static int sum(int n){
+        if (n<=1)   return 1;
+        return n+sum(n-1);
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int result = sum(n);
+        System.out.println(result);
+    }
+
+
+}
