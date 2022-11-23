6243797e06726cb3b453ac7ae0298edd1e2a65af
lucyoz <lucyoz.kwon@gmail.com>
2022-11-23 09:33:24 +0900

    [ADD]재귀: CodeUp 1854 - 재귀로 자릿수의 합 구하기

---
+++ src/main/java/recursive/CodeUp1854.java
@@ -0,0 +1,22 @@
+package recursive;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CodeUp1854 {
+    public static long sum(long n){
+        if(n/10==0) return n;
+        return n%10+sum(n/10);
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+
+        long result = sum(n);
+        System.out.println(result);
+    }
+
+}
