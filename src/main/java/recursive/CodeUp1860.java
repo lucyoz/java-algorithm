e1eab348de92979feb35577e6beea550fdb12a03
lucyoz <lucyoz.kwon@gmail.com>
2022-11-24 09:32:05 +0900

    docs: 불필요한 주석 제거

--- src/main/java/recursive/CodeUp1860.java
+++ src/main/java/recursive/CodeUp1860.java
@@ -4,25 +4,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class CodeUp1859 {
-    //별을 찍는 메소드
-    public static void printStar(int n){
-        if (n<=0) return;
-        printStar(n-1);
-        System.out.print("*");
+public class CodeUp1860 {
+    //숫자 출력
+    public static void printNum(int n){
+        if(n<=0)    return;
+        printNum(n-1);
+        System.out.print(n+" ");
     }
 
-    public static void print(int n){
-        if(n<=0)    return;
-        print(n-1);
-        printStar(n);
-        System.out.println();
-    }
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-        print(n);
+
+
     }
 }
