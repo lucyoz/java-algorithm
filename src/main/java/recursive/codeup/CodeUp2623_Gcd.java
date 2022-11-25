c1b2b2880598953319aee5e3f2d01387b6575569
lucyoz <lucyoz.kwon@gmail.com>
2022-11-25 09:31:42 +0900

    feat: 재귀로 최대공약수 구하기  - CodeUp 2623번

---
+++ src/main/java/recursive/codeup/CodeUp2623_Gcd.java
@@ -0,0 +1,29 @@
+package recursive.codeup;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CodeUp2623_Gcd {
+    public static int getGcd(int a, int b){
+        if (a == b) return a;
+        else {
+            int min = a > b ? b : a;
+            int max = a < b ? b : a;
+            System.out.printf("%d, %d\n",max-min, min);
+            return getGcd(max-min, min);
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int a = Integer.parseInt(str.split(" ")[0]);
+        int b = Integer.parseInt(str.split(" ")[1]);
+
+        int result = getGcd(a, b);
+        System.out.println("최대공약수: "+result);
+
+    }
+}
