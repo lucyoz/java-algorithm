7a9979240baac97707dcd98c82ab45a4e8875ce0
lucyoz <lucyoz.kwon@gmail.com>
2022-11-29 09:34:35 +0900

    feat: 기수정렬 실습1(0~9 수 정렬하기)

---
+++ src/main/java/Sort/radixSort/RadixSort1.java
@@ -0,0 +1,27 @@
+package Sort.radixSort;
+
+import java.util.Arrays;
+
+public class RadixSort1 {
+    public static void main(String[] args) {
+        int[] arr = {7, 4, 5, 9, 1, 0};
+
+        int[] radixArr = new int[10];
+        Arrays.fill(radixArr, -1);
+
+        for(int i:arr){
+            radixArr[i] = i;
+        }
+
+        System.out.println(Arrays.toString(radixArr));
+
+        int cnt = 0;
+        for (int i = 0; i < radixArr.length; i++) {
+            if(radixArr[i]!=-1){
+                arr[cnt++] = radixArr[i];
+            }
+
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
