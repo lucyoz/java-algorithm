96afef44ed5747f1358d37a58cf818e364a2c7c2
lucyoz <lucyoz.kwon@gmail.com>
2022-12-08 09:24:08 +0900

    feat: DP를 이용한 fibonacci 수열

---
+++ src/main/java/dynamic/Fibonacci.java
@@ -0,0 +1,21 @@
+package dynamic;
+
+public class Fibonacci {
+    public static int fiboDP(int n){
+        int[] memo = new int[n+1];
+
+        memo[0] = 1;
+        memo[1] = 1;
+        if (n>=2){
+            for (int i = 2; i < n+1; i++) {
+                memo[i] = memo[i-1]+memo[i-2];
+            }
+        }
+
+        return memo[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fiboDP(7));
+    }
+}
