65860e4f56c24491dbace10beda6f1bc4c319adf
lucyoz <lucyoz.kwon@gmail.com>
2022-12-08 10:21:28 +0900

    feat: DP를 이용한 fibonacci (n=0일 때 수정)

--- src/main/java/dynamic/Fibonacci.java
+++ src/main/java/dynamic/Fibonacci.java
@@ -4,10 +4,10 @@ public class Fibonacci {
     public static int fiboDP(int n){
         int[] memo = new int[n+1];
 
-        memo[0] = 1;
         memo[1] = 1;
+        memo[2] = 1;
         if (n>=2){
-            for (int i = 2; i < n+1; i++) {
+            for (int i = 3; i < n+1; i++) {
                 memo[i] = memo[i-1]+memo[i-2];
             }
         }
